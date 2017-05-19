58734f4cd600cc9ec69217c5310e5923d6278f91
cuscedu <cuscedu@mac07.local>
2017-05-19 15:58:31 +0700

    v2

--- BaiTap4/ViewController.swift
+++ BaiTap4/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    //v1
+    //v2
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
